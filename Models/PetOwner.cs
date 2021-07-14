f6cbb351bac9aea35cf5c1f9f1e8b117d0dd916e
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-14 15:14:40 -0500

    Deleted Target per Dane

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,21 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner
+    {
+        // EF knows that id is the Primary Key, EF knows that is Serial
+        public int id {get; set;}
+
+        [Required]
+
+        public string name {get; set;}
+
+
+        [Required]
+
+        public string emailAddress {get; set;}
+
+        [NotMapped]
+        public int petCount {get; set;}
+    }
 }
